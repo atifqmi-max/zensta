bfdb6765b8e1d1423781f1a1b1dee3cdb8c3591a
atifqmi-max <atifqmi@gmail.com>
2026-02-22 10:25:55 +0500

    Create Notification.js

---
+++ beckend/models/Notification.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const NotificationSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['like', 'comment', 'follow', 'mention', 'admin'],
+    required: true
+  },
+  title: String,
+  message: String,
+  data: {
+    type: mongoose.Schema.Types.Mixed
+  },
+  read: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Notification', NotificationSchema);
